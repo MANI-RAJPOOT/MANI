6173077d24b239ec61440d132c2fa8d829a1c70c
MaNi RaJpOoT <113978329+MANI-RAJPOOT@users.noreply.github.com>
2023-06-06 15:06:02 +0500

    Update MANI.py

--- MANI.py
+++ MANI.py
@@ -1,3 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
 import os,platform
 
 os.system('git pull')
@@ -6,8 +19,12 @@ MANI=platform.architecture()[0]
 
 if MANI=="32bit":
 
-    print('\033[1;32m [×] \033[1;31mSorry 32Bit Device Not Supported This Tool...!\n\033[1;32m [√] \033[1;33mInshallah Coming Soon...!')
+	
+
+	__import__("MANI32")
 
 elif MANI=="64bit":
+
+	
 
     __import__("MANI")
